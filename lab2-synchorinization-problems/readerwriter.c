2b0d5915a45055b3a4b9540d93a8724a88144c28
lilixac <hinewton25@gmail.com>
2020-11-21 14:35:49 +0545

    Bugs fixed, spaces indented properly

--- lab2-synchorinization-problems/readerwriter.c
+++ lab2-synchorinization-problems/readerwriter.c
@@ -10,7 +10,7 @@ struct semaphore
     bool wrt;
 };
 
-voidaddR(struct semaphore *s)
+void addR(struct semaphore *s)
 {
     if (s->mutex == 0 && s->rcount == 0)
     {
@@ -25,7 +25,7 @@ voidaddR(struct semaphore *s)
     return ;
 }
 
-voidaddW(struct semaphore *s)
+void addW(struct semaphore *s)
 {
     if(s->mutex==1)
     {
@@ -34,11 +34,11 @@ voidaddW(struct semaphore *s)
         printf("\nWriter Process added.\n");
     }
     else if(s->wrt) printf("\nSorry, Writer already operational.\n");
-    elseprintf("\nSorry, File open in Read mode.\n");
+    else printf("\nSorry, File open in Read mode.\n");
     return ;
 }
 
-voidremR(struct semaphore *s)
+void remR(struct semaphore *s)
 {
     if(s->rcount == 0) printf("\nNo readers to remove.\n");
     else
@@ -49,7 +49,7 @@ voidremR(struct semaphore *s)
     return ;
 }
 
-voidremW(struct semaphore *s)
+void remW(struct semaphore *s)
 {
     if(s->wrt==0) printf("\nNo Writer to Remove");
     else
@@ -72,9 +72,10 @@ int main()
     struct semaphore S1={1,0,0};
     while(1)
     {
-        system("cls");
-        printf("Options :-\n1.Add Reader.\n2.Add Writer.\n3.Remove Reader.\n4.Remove
-                Writer.\n5.Exit.\n\n\tChoice : ");intch;
+        // system("cls");
+        printf("Options :-\n1.Add Reader.\n2.Add Writer.\n3.Remove Reader.\n4.Remove \
+                Writer.\n5.Exit.\n\n\tChoice : ");
+        int ch;
         scanf("%d",&ch);
         switch(ch)
         {
@@ -82,10 +83,12 @@ int main()
             case 2: addW(&S1); break;
             case 3: remR(&S1); break;
             case 4: remW(&S1); break;
-            case 5: printf("\n\tGoodBye!"); getch(); return 0;
+            case 5: printf("\n\tGoodBye!"); 
+            getch(); 
+            return 0;
             default: printf("\nInvalid Entry!"); continue;
         }
-        printf("\n\n<<<<<< Current Status >>>>>>\n\n\tMutex\t\t:\t%d\n\tActive
+        printf("\n\n<<<<<< Current Status >>>>>>\n\n\tMutex\t\t:\t%d\n\tActive\
                 Readers\t:\t%d\n\tWaiting Readers\t:\t%d\n\tWriter Active\t:\t%s\n\n", S1.mutex,
                 S1.rcount, S1.rwait, (S1.mutex==0 && S1.rcount==0) ? "YES" : "NO");
         system("pause");
